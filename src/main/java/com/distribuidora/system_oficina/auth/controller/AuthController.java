4c2aed5eb0e61d88acda5fd09c345b4456a6973a
Erick-008 <erick.s0usa@outlook.com>
2026-06-09 10:38:48 -0300

    configurando controller

--- src/main/java/com/distribuidora/system_oficina/auth/controller/AuthController.java
+++ src/main/java/com/distribuidora/system_oficina/auth/controller/AuthController.java
@@ -1,5 +1,12 @@
 package com.distribuidora.system_oficina.auth.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/login")
 public class AuthController {
     
+ 
 }
